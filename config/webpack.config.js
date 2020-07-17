f23bdb07f1acf57122ece3335ef74afb649e1068
zhiping.li <276340452@qq.com>
2020-07-17 11:16:29 +0800

    chore: 新增代码分析工具

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -25,6 +25,8 @@ const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 const postcssNormalize = require('postcss-normalize');
 
 const MergeJsonWebpackPlugin = require('merge-jsons-webpack-plugin');
@@ -692,6 +694,7 @@ module.exports = function(webpackEnv) {
               { pattern: "src/i18n/zh-cn/*.json", fileName: "./i18n/zh-cn.json" }
           ]}
         }),
+        new BundleAnalyzerPlugin(),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell webpack to provide empty mocks for them so importing them works.
