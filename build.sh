221545abccd1c01091f2d082e1867e62acaaa251
zhiping.li <276340452@qq.com>
2020-07-27 13:41:27 +0800

    fix: 参数更新

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#npm config set registry http://npm.yematech.cn/
+npm config set registry http://npm.yematech.cn/
 npm install
-npx cross-env PUBLIC_URL= API_URL=  npm run env:dev && npm run build
+npx cross-env PUBLIC_URL=$1 API_URL=$2 npm run env:dev && npm run build
